ebcb5af7b0f6e17818d13d9cf5d4d30132c3c047
Elfelins <50467996+Elfelins@users.noreply.github.com>
2023-10-04 23:41:19 +0200

    Update script.js
    Generowanie listy tytułów

--- script.js
+++ script.js
@@ -35,8 +35,41 @@ function titleClickHandler(event){
   targetArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+function generateTitleLinks(){
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+  /* find all the articles and save them to variable: articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html = '';
+
+  for(let article of articles){
+    /* get the article id */
+    const articleId = article.getAttribute("id");
+
+    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+    /* insert link into html variable */
+    html = html + linkHTML;
+    console.log('htmlLink:', linkHTML);  
+  }
+
+  titleList.innerHTML = html;
+  const links = document.querySelectorAll('.titles a');
+
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }    
 }
+
+generateTitleLinks();
